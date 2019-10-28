854d64eef0937ae202a98ea7b95e8ac33dbc3ff3
wbodo <55714242+wbodo@users.noreply.github.com>
2019-10-28 21:41:09 +0100

    Moving everything into rec_to_binaries subdirectory

--- rec_to_binaries/setup.py
+++ rec_to_binaries/setup.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-
-from setuptools import find_packages, setup
+import os
+import setuptools
 
 INSTALL_REQUIRES = ['numpy', 'pandas', 'mountainlab-pytools']
 TESTS_REQUIRE = ['pytest >= 2.7.1']
 
-setup(
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+setuptools.setup(
     name='rec_to_binaries',
+    setup_requires=["wheel"],
     version='0.1.1.dev0',
     license='MIT',
     description=('Covert SpikeGadgets rec files to binaries'),
     author='Eric Denovellis, Daniel Liu',
     author_email='Eric.Denovellis@ucsf.edu',
-    url='https://github.com/LorenFrankLab/rec_to_binaries',
-    packages=find_packages(),
+    url='https://github.com/wbodo/rec_to_binaries',
+    packages=setuptools.find_packages(),
     install_requires=INSTALL_REQUIRES,
-    python_requires='>3.6',
     tests_require=TESTS_REQUIRE,
 )
