7cc4c7708262aaa5d7dc21237266f55cc31d8262
wbodo <55714242+wbodo@users.noreply.github.com>
2019-10-28 21:28:31 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from setuptools import find_packages, setup
+
+INSTALL_REQUIRES = ['numpy', 'pandas', 'mountainlab-pytools']
+TESTS_REQUIRE = ['pytest >= 2.7.1']
+
+setup(
+    name='rec_to_binaries',
+    version='0.1.1.dev0',
+    license='MIT',
+    description=('Covert SpikeGadgets rec files to binaries'),
+    author='Eric Denovellis, Daniel Liu',
+    author_email='Eric.Denovellis@ucsf.edu',
+    url='https://github.com/LorenFrankLab/rec_to_binaries',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    python_requires='>3.6',
+    tests_require=TESTS_REQUIRE,
+)
